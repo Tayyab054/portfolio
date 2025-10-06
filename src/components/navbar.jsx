402b24f05cc16ee15336622b27582be13a855ac7
Tayyab ali <2024cs491@student.uet.edu.pk>
2025-10-06 22:47:06 +0500

    Update navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 function Navbar() {
   return (
-    <nav className="fixed left-0 w-full bg-black  text-white shadow-md z-50 px-6 md:px-18 lg:px-32">
-      <div className="container flex px-2 justify-center md:justify-between items-center mx-auto">
+    <nav className="fixed  w-full bg-black  text-white shadow-md py-2 md:px-18 lg:px-32">
+      <div className="container flex mx-4 justify-center md:justify-between items-center mx-auto">
         <div className=" text-2xl font-bold hidden md:inline ">Tayyab Ali</div>
         <div className="space-x-6 ">
           <div><img src="" alt="" /></div>
-          <a href="#hero" className="hover:text-teal-300">Home</a>
+          <a href="#hero" className="hover:text-teal-300 hidden sm:inline">Home</a>
           <a href="#About" className="hover:text-teal-300">About</a>
           <a href="#Servecies" className="hover:text-teal-300">Servecies</a>
           <a href="#Projects" className="hover:text-teal-300">Projects</a>
